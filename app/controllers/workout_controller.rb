90018742b04494d8b9ee7ecd7c5477354f31006a
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-02-25 00:58:31 +0000

    Added some basic edit functionality

--- app/controllers/workout_controller.rb
+++ app/controllers/workout_controller.rb
@@ -1,11 +1,30 @@
-class WorkoutController < Sinatra::Base
-  register Sinatra::ActiveRecordExtension
-  set :session_secret, "super_sekrit"
-  set :views, Proc.new { File.join(root, "../views/") }
-  enable :sessions
+class WorkoutController < ApplicationController
 
   get '/workouts/:id' do
-    @workout = Workout.find(params[:id])
-    erb :'/workouts/show'
+    if logged_in?
+      @workout = Workout.find(params[:id])
+      erb :'/workouts/show'
+    else 
+      redirect '/users/login'
+    end 
   end
+
+  get '/workouts/:id/edit' do 
+    if logged_in?
+      @workout = Workout.find(params[:id])
+      erb :'/workouts/edit'
+    else  
+      redirect '/users/login'
+    end 
+  end 
+
+  patch '/workouts/:id' do 
+    @workout = Workout.find(params[:id])
+    @workout.update(date: params[:date]) if !params[:date].empty?
+    @workout.update(notes: params[:notes]) if !params[:notes].empty?
+    redirect "/workouts/#{@workout.id}"
+  end 
+    
+
+  
 end 
