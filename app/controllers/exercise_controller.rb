724f63df2858a0c270a3c47f2dc4089a264c1ef7
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-02-26 04:36:30 +0000

    Fixed new exercise validation - requires name and bodypart parameters now

--- app/controllers/exercise_controller.rb
+++ app/controllers/exercise_controller.rb
@@ -9,14 +9,17 @@ class ExerciseController < ApplicationController
   end 
 
   post '/exercises' do 
-    if logged_in? && params[:exercise][:name] && params[:exercise][:bodypart]
+    if logged_in? && !params[:exercise][:name].empty? && !params[:exercise][:bodypart].empty?
       if Exercise.find_by(name: params[:exercise][:name])
         flash[:message] = "An exercise with that name already exists!"
         redirect '/exercises/new'
-      else 
-        e = Exercise.create(params[:exercise])
+      else
+        e = Exercise.create(name: params[:exercise][:name], bodypart: params[:exercise][:bodypart])
         redirect '/exercises'
       end
+    elsif params[:exercise][:name].empty? || params[:exercise][:bodypart].empty?
+      flash[:message] = "Your exercise must have a name and bodypart!"
+      redirect '/exercises/new'
     else 
       redirect '/login'
     end 
