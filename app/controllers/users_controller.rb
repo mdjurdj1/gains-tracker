12231b78782a10d03086fe562dd101e63989847e
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-02-26 05:01:03 +0000

    Added functional signup system with input validation checks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,27 @@ class UserController < ApplicationController
   end
 
   get '/signup' do
-    erb :'/users/signup'
+    if logged_in?
+      redirect '/home'
+    else
+      erb :'/users/signup'
+    end
+  end
+
+  post '/signup' do
+    if User.find_by(username: params[:username])
+      flash[:message] = "That username is taken, please try another one!"
+      redirect '/signup'
+    else
+      @user = User.new(username: params[:username], password: params[:password], email: params[:email])
+        if @user.save
+          session[:user_id] = @user.id
+          redirect '/home'
+        else
+          flash[:message] = "Invalid password(6 character minimum) or email given. Please try again!"
+          redirect '/signup'
+        end
+      end
   end
 
   get '/logout' do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 #Let's throw the Rock into our DB. The Rock just had a chest day.
-dwayne = User.create(username: "The Rock", email: "Therocksays@gmail.com", password: "abc123")
+dwayne = User.create(username: "DwayneJ", email: "Therocksays@gmail.com", password: "abc123")
 workout_1 = Workout.create(name: "Chest/Tris", notes: "Had a good workout today. Can add weight to bench next time.", date: "2017-02-18")
 workout_1.workout_exercises << WorkoutExercise.create(workout_id: 1, exercise_id: 2)
 workout_1.workout_exercises << WorkoutExercise.create(workout_id: 1, exercise_id: 7)
@@ -13,7 +13,7 @@ workout_2.workout_exercises << WorkoutExercise.create(workout_id: 2, exercise_id
 dwayne.workouts << workout_2
 dwayne.save
 
-alexa = User.create(username: "Lexis", email: "Alexaa@yahoo.com", password: "abc123")
+alexa = User.create(username: "LexyFlexxy", email: "Alexaa@yahoo.com", password: "abc123")
 workout_3 = Workout.create(name: "Legs", notes: "Felt really tired when I got to the gym today. Could use more sleep!", date: "2017-01-15")
 workout_3.workout_exercises << WorkoutExercise.create(workout_id: 3, exercise_id: 1)
 workout_3.workout_exercises << WorkoutExercise.create(workout_id: 3, exercise_id: 9)
@@ -22,13 +22,17 @@ alexa.save
 
 
 #Let's create some exercises for the database
+Exercise.create(name: "Flat Bench Press", bodypart: "Chest")
+Exercise.create(name: "Incline Bench Press", bodypart: "Chest")
+Exercise.create(name: "Chest Flies", bodypart: "Chest")
+Exercise.create(name: "Cable Crossover", bodypart: "Chest")
 Exercise.create(name: "Squat", bodypart: "Legs")
-Exercise.create(name: "Bench Press", bodypart: "Chest")
+Exercise.create(name: "Leg Press", bodypart: "Legs")
+Exercise.create(name: "Lunges", bodypart: "Legs")
 Exercise.create(name: "Deadlift", bodypart: "Back")
-Exercise.create(name: "Overhead Press", bodypart: "Shoulders")
 Exercise.create(name: "T-Bar Row", bodypart: "Back")
+Exercise.create(name: "Overhead Press", bodypart: "Shoulders")
+Exercise.create(name: "Side Raises", bodypart: "Shoulders")
 Exercise.create(name: "Barbell Curls", bodypart: "Biceps")
 Exercise.create(name: "Dumbell Curls", bodypart: "Biceps")
 Exercise.create(name: "Tricep Extensions", bodypart: "Triceps")
-Exercise.create(name: "Chest Flies", bodypart: "Chest")
-Exercise.create(name: "Leg Press", bodypart: "Legs")
