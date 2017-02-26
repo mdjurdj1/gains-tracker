49e0ee52a1ff83a53ce6e51b1101b3c8b289e1bd
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-02-26 00:25:10 +0000

    Added div containers for each page

--- app/controllers/exercise_controller.rb
+++ app/controllers/exercise_controller.rb
@@ -8,4 +8,18 @@ class ExerciseController < ApplicationController
     erb :'exercises/new'
   end 
 
+  post '/exercises' do 
+    if logged_in? && params[:exercise][:name] && params[:exercise][:bodypart]
+      if Exercise.find_by(name: params[:exercise][:name])
+        flash[:message] = "An exercise with that name already exists!"
+        redirect '/exercises/new'
+      else 
+        e = Exercise.create(params[:exercise])
+        redirect '/exercises'
+      end
+    else 
+      redirect '/login'
+    end 
+  end 
+
 end  
